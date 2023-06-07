8aadc03b00003e3a0cfba5da68c42ab76bb78683
Steven G. Opferman <steven.g.opferman@gmail.com>
2023-06-07 09:00:55 -0700

    code for generating confusion matrix from csv

---
+++ API Testing/generate_confusion_matrix.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+
+DATASET_PATH = "Confusion_Matrix_Data/2023-06-06_20-46-20_perspective.csv"
+
+
+if __name__ == "__main__":
+    # Read the CSV file into a pandas DataFrame
+    data = pd.read_csv(DATASET_PATH)
+
+    # Extract the actual and predicted labels from the DataFrame
+    actual_labels = data['ground_truth']
+    predicted_labels = data['flagged']
+
+    # Create the confusion matrix
+    cm = confusion_matrix(actual_labels, predicted_labels)
+
+    # Print the confusion matrix
+    print(cm)
